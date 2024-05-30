ff909d5d61d3e3578c0610bfc2194f974ea35437
Sjoerd van de Wege <s.vandewege@student.han.nl>
2024-05-30 12:00:33 +0200

    removed obj files

--- EmbeddedSoftMainProgram/obj/bme280.d
+++
@@ -1,3 +0,0 @@
-./obj/bme280.o: bme280\bme280.c bme280\bme280.h \
-  C:\Software\Keil_v5\ARM\ARMCLANG\include\stdint.h \
-  C:\Software\Keil_v5\ARM\ARMCLANG\include\stdbool.h i2c1\i2c1.h
--- EmbeddedSoftMainProgram/obj/delay.d
+++
@@ -1,2 +0,0 @@
-./obj/delay.o: delay\delay.c delay\delay.h \
-  C:\Software\Keil_v5\ARM\ARMCLANG\include\stdint.h
