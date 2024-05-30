1b2cbfa2c80e260f2941e30b58d314014d73a6bc
Sjoerd van de Wege <s.vandewege@student.han.nl>
2024-05-30 10:27:02 +0200

    feat(eeprom):Badded data structures for Page writes on eeprom

---
+++ EmbeddedSoftMainProgram/logging/logging.h
@@ -0,0 +1,18 @@
+#ifndef LOGGING_H
+#define LOGGING_H
+#include <stdint.h>
+
+struct __attribute__((packed)) LogData {
+  uint32_t lattitude;
+  uint32_t longtitude;
+  uint32_t temperature;
+  uint32_t humidity;
+};
+
+struct __attribute__((packed)) MetaData {
+  uint32_t countOfRecords;
+  uint32_t pad32;
+  uint64_t pad64;
+};
+
+#endif // LOGGING_H
